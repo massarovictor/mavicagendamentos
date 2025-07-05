66ba3011cf4df34cb0e3da70b2bdca0d4f22c4ae
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-07-05 15:36:34 -0300

    fix: adding chart colors in tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,13 @@ export default {
 				ring: 'hsl(var(--ring))',
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
+				chart: {
+					1: 'hsl(var(--chart-1))',
+					2: 'hsl(var(--chart-2))',
+					3: 'hsl(var(--chart-3))',
+					4: 'hsl(var(--chart-4))',
+					5: 'hsl(var(--chart-5))',
+				},
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
 					foreground: 'hsl(var(--primary-foreground))'
